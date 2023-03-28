eff538ab11856bd74ead362ca29f00789486f25f
RoterDamon <80481177+RoterDamon@users.noreply.github.com>
2023-03-28 18:04:12 +0300

    Add files via upload

--- lab1/cache.py
+++ lab1/cache.py
@@ -1,67 +1,23 @@
-import string
-
-
-class Node:
-    def __init__(self, key=None, value=None):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
-
-
 class LRUCache:
     def __init__(self, capacity: int = 10) -> None:
-        if capacity <= 0:
-            raise ValueError("cache size must be greater than 0")
-        else:
-            self.capacity = capacity
-            self.cache = {}
-            self.head = Node()
-            self.tail = Node()
-            self.head.next = self.tail
-            self.tail.prev = self.head
+        self.const = 10
+        self.dict = {}
 
     def get(self, key: str) -> str:
-        result: string
-
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-            self._add(node)
-            result = node.value
-        else:
-            result = ""
-
-        return result
+        return self.dict.get(key, "")
 
     def set(self, key: str, value: str) -> None:
-        if key in self.cache:
-            self._remove(self.cache[key])
+        if key in self.dict:
+            del self.dict[list(self.dict)[0]]
+            self.dict[key] = value
+        elif len(self.dict) > self.const - 1:
+            del self.dict[list(self.dict)[0]]
+            self.dict[key] = value
+        else:
+            self.dict[key] = value
 
-        node = Node(key, value)
-        self.cache[key] = node
-        self._add(node)
+    def rem(self, key: str) -> None:
+        return self.set(key, "")
 
-        if len(self.cache) > self.capacity:
-            node = self.head.next
-            self._remove(node)
-            del self.cache[node.key]
-
-    def remove(self, key: str) -> None:
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-            del self.cache[key]
-
-    def _remove(self, node):
-        prev = node.prev
-        next = node.next
-        prev.next = next
-        next.prev = prev
-
-    def _add(self, node):
-        prev = self.tail.prev
-        prev.next = node
-        node.prev = prev
-        node.next = self.tail
-        self.tail.prev = node
+    def getall(self) -> None:
+        return print(self.dict)
--- lab1/main.py
+++ lab1/main.py
@@ -3,10 +3,12 @@ from cache import LRUCache
 
 print("Python version: {}.{}".format(sys.version_info.major, sys.version_info.minor))
 
-cache = LRUCache(100)
+cache = LRUCache()
 cache.set('Jesse', 'Pinkman')
 cache.set('Walter', 'White')
 cache.set('Jesse', 'James')
-print(cache.get('Jesse')) # вернёт 'James'
-cache.remove('Walter')
-print(cache.get('Walter')) # вернёт ''
+cache.get('Jesse') # вернёт 'James'
+cache.rem('Walter')
+cache.get('Walter') # вернёт ''
+
+cache.getall()
