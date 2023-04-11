9845cc1ce8a42f48fe1ca64f8f515ccd77f081f2
RoterDamon <80481177+RoterDamon@users.noreply.github.com>
2023-04-11 10:34:43 +0300

    Update views.py

--- lab3/crm/views.py
+++ lab3/crm/views.py
@@ -1,27 +1,56 @@
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+
 
 # Create your views here.
 
 @require_http_methods(["GET", "POST"])
-
 def index(request):
     return render(request, "home.html")
 
 
 def art_shop_profile(request):
-    return JsonResponse({'Stub': 'Profile stub!'})
+    user_profile = {
+        'username': 'Kate',
+        'email': 'kate@gmail.com',
+        'age': 21,
+        'gender': 'female',
+        'location': 'Russia'
+    }
+    return JsonResponse(user_profile)
 
 
 def arts_list(request):
-    return JsonResponse({'Stub': 'Stub for the list of works of arts!'})
+    if request.method == "GET":
+        art_id_1 = request.GET.get("art_id", 1)
+        pic_1 = request.GET.get("name", "Morning in the pine forest")
+        artist_1 = request.GET.get("artist", "Ivan Ivanovich Shishkin")
+        genre_1 = request.GET.get("genre", "Landscape")
+        art_id_2 = request.GET.get("art_id", 2)
+        name_2 = request.GET.get("name", "In the Blue Expanse")
+        artist_2 = request.GET.get("artist", "Arkady Alexandrovich Rylov")
+        genre_2 = request.GET.get("genre", "Seascape")
+        return JsonResponse({"All arts": [{"art_id": art_id_1, "name": pic_1, "artist": artist_1, "genre": genre_1},
+                                          {"art_id": art_id_2, "name": name_2, "artist": artist_2, "genre": genre_2}]})
+    else:
+        return HttpResponseBadRequest("<h2>Invalid request method!</h2>")
 
 
 def categories(request):
-    return JsonResponse({'Stub': 'Stub for category page!'})
+    if request.method == 'GET':
+        genre_categories = ['Landscape', 'Seascape', 'Portrait', 'Still-life']
+        return JsonResponse(genre_categories, safe=False)
+    else:
+        return HttpResponseBadRequest("<h2>Invalid request method!</h2>")
 
 
-def category(request, id_category):
-    return JsonResponse({'Stub': 'Stub for category №%s' % id_category})
+@csrf_exempt  # разрешаем делать POST запрос без куки
+def add_genre(request):
+    if request.method == "POST":
+        genre_id = request.GET.get("genre_id", 5)
+        name = request.GET.get("name", "Historical")
+        return JsonResponse({"genre_id": genre_id, "name": name})
+    else:
+        return HttpResponseBadRequest("<h2>Invalid request method!</h2>")
