a68acdd6a85ff8880c74d150eebcf5b247448f0e
RoterDamon <80481177+RoterDamon@users.noreply.github.com>
2023-04-11 10:09:24 +0300

    update chace.py

--- lab1/cache.py
+++ lab1/cache.py
@@ -1,6 +1,6 @@
 class LRUCache:
     def __init__(self, capacity: int = 10) -> None:
-        self.const = 10
+        self.capacity = capacity
         self.dict = {}
 
     def get(self, key: str) -> str:
@@ -8,16 +8,17 @@ class LRUCache:
 
     def set(self, key: str, value: str) -> None:
         if key in self.dict:
-            del self.dict[list(self.dict)[0]]
+            del self.dict[key]
             self.dict[key] = value
-        elif len(self.dict) > self.const - 1:
-            del self.dict[list(self.dict)[0]]
+        elif len(self.dict) > self.capacity - 1:
+            del self.dict.pop[next(iter(self.dict))]
             self.dict[key] = value
         else:
             self.dict[key] = value
 
     def rem(self, key: str) -> None:
-        return self.set(key, "")
+        if key in self.dict:
+            del self.dict[key]
 
     def getall(self) -> None:
         return print(self.dict)
